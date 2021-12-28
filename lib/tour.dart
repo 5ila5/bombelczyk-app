fe9d7d4976295c67209121647a24249fb62881c9
5ila5 <silaskoch@gmx.de>
2021-12-28 17:54:51 +0100

    added Tour to Bottom Navigation Bar (not Funktunal yet)

---
+++ lib/tour.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Tour extends StatefulWidget {
+  Tour({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  TourState createState() => TourState();
+}
+
+class TourState extends State<Tour> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("Platzhalter");
+  }
+
+}

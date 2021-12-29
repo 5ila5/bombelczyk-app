33979f90bf2565ce3a3d503642f61f432a60fd97
5ila5 <5ila5@users.noreply.github.com>
2021-12-29 19:12:29 +0100

    tagestour works now, cleaned main.dart

--- lib/web_comunicater.dart
+++ lib/web_comunicater.dart
@@ -1,13 +1,29 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
 
 class WebComunicater {
   static final String _ipToAsk = 'bombelczyk-aufzuege.de';
   static final gzip = GZipCodec();
+  static SharedPreferences prefs;
 
   static Future<String> sendRequest(Map<String, String> body,
       {bool login = false}) async {
+
+    if (Preferences.prefs == null) {
+      prefs = await Preferences.initPrefs();
+    } else {
+      prefs = Preferences.prefs;
+    }
+    if (!prefs.containsKey("key")) {
+      return null;
+    }
+
+    body.addAll({'auth': prefs.getString("key")});
     print(base64.encode(gzip.encode(utf8.encode(jsonEncode(body)))));
     http.Response response = await http.post(
       Uri.https(_ipToAsk,

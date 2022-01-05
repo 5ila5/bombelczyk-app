2cd3a13d1e03c0228d4f5e9ca2acb18a01f3cbfb
5ila5 <5ila5@users.noreply.github.com>
2022-01-05 16:30:07 +0100

    fixed a Bug Where you could not login

--- lib/web_comunicater.dart
+++ lib/web_comunicater.dart
@@ -4,8 +4,6 @@ import 'package:http/http.dart' as http;
 import 'helper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-
 class WebComunicater {
   static final String _ipToAsk = 'bombelczyk-aufzuege.de';
   static final gzip = GZipCodec();
@@ -13,17 +11,17 @@ class WebComunicater {
 
   static Future<String> sendRequest(Map<String, String> body,
       {bool login = false}) async {
-
     if (Preferences.prefs == null) {
       prefs = await Preferences.initPrefs();
     } else {
       prefs = Preferences.prefs;
     }
-    if (!prefs.containsKey("key")) {
+    if (!prefs.containsKey("key") && !login) {
       return null;
     }
-
-    body.addAll({'auth': prefs.getString("key")});
+    if (!login) {
+      body.addAll({'auth': prefs.getString("key")});
+    }
     print(base64.encode(gzip.encode(utf8.encode(jsonEncode(body)))));
     http.Response response = await http.post(
       Uri.https(_ipToAsk,
